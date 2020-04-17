2f4f1a810d19f9860147326ad99d944b7a7f64d6
Sam Caulfield <sam@samcaulfield.com>
2020-04-17 20:28:07 +0100

    Clean up setup.bash a bit

--- setup.bash
+++ setup.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #
+# Configure Git
+#
+
+ln -s $(pwd)/.gitconfig $HOME/.gitconfig
+
+#
 # Configure Vim
 #
 
@@ -15,14 +21,18 @@ git clone https://github.com/preservim/nerdtree.git $VIM_PACKAGES_DIR/nerdtree
 ( cd $VIM_PACKAGES_DIR/nerdtree && git checkout 6.7.7 )
 
 #
+# Configure X
+#
+
+ln -s $(pwd)/.xinitrc ~/.xinitrc
+ln -s $(pwd)/.Xresources ~/.Xresources
+
+#
 # Configure everything else
 #
 
-ln -s $(pwd)/newl.bash ~/newl
 [ ! -d ~/.config ] && mkdir ~/.config
 ln -s $(pwd)/user-dirs.dirs ~/.config/user-dirs.dirs
-ln -s $(pwd)/.xinitrc ~/.xinitrc
-ln -s $(pwd)/.Xresources ~/.Xresources
-ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/newl.bash ~/newl
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/awesome ~/.config/awesome
