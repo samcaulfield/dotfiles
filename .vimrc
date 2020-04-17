300353d3627adcb8d428df8f96abe5b062f470aa
Sam Caulfield <sam@samcaulfield.com>
2020-04-17 20:24:14 +0100

    Use Vim 8's built-in package manager

--- .vimrc
+++ .vimrc
@@ -1,5 +1,3 @@
-execute pathogen#infect()
-
 colorscheme industry
 
 " Highlight trailing whitespace
--- setup.bash
+++ setup.bash
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+#
+# Configure Vim
+#
+
+ln -s $(pwd)/.vimrc $HOME/.vimrc
+
+VIM_PACKAGES_DIR=$HOME/.vim/pack/sc/start
+mkdir -p $VIM_PACKAGES_DIR
+
+git clone https://github.com/airblade/vim-gitgutter.git $VIM_PACKAGES_DIR/vim-gitgutter
+
+git clone https://github.com/preservim/nerdtree.git $VIM_PACKAGES_DIR/nerdtree
+( cd $VIM_PACKAGES_DIR/nerdtree && git checkout 6.7.7 )
+
+#
+# Configure everything else
+#
+
 ln -s $(pwd)/newl.bash ~/newl
 [ ! -d ~/.config ] && mkdir ~/.config
 ln -s $(pwd)/user-dirs.dirs ~/.config/user-dirs.dirs
-ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.xinitrc ~/.xinitrc
 ln -s $(pwd)/.Xresources ~/.Xresources
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/awesome ~/.config/awesome
-
-mkdir -p ~/.vim/autoload
-mkdir -p ~/.vim/bundle
-
-wget -nc https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim  -P ~/.vim/autoload
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
-git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-( cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py --clang-completer )
-
-echo "[flake8]
-max-line-length = 200" > ~/.config/flake8
-
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-
